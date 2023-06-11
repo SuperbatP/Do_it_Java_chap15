37935ebffb8ab5667c6a9d9277085d57fd6d8a60
SuperbatP <wjssnfl0504@gmail.com>
2023-06-11 20:40:21 +0900

    쓰레드의 데몬 설정-일반 쓰레드

---
+++ thread/src/sec03/_threadproperties/EX03/ThreadProperties_3_1.java
@@ -0,0 +1,35 @@
+package sec03._threadproperties.EX03;
+
+class MyThread extends Thread{
+	@Override
+	public void run() {
+		System.out.println(getName()+" : "+(isDaemon()? "데몬쓰레드":"일반쓰레드"));
+		for(int i=0; i<6; i++) {
+			System.out.println(getName()+" : "+ i +"초");
+			try{Thread.sleep(1000);} catch(InterruptedException e) {}
+		}
+	}
+}
+
+public class ThreadProperties_3_1 {
+public static void main(String[] args) {
+	//일반쓰레드
+	Thread thread1=new MyThread();
+	thread1.setDaemon(false);//디폴트
+	thread1.setName("thread1");
+	thread1.start();
+	
+	//데몬쓰레드
+//	Thread thread2=new MyThread();
+//	thread2.setDaemon(true);
+//	thread2.setName("thread2");
+//	thread2.start();
+	
+	//3초후 main 쓰레드 종료
+	try {Thread.sleep(3100);
+	} catch (InterruptedException e) {}
+	System.out.println("메인 쓰레드 종료");
+	}
+	
+}
+
